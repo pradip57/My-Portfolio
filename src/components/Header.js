aac30d3afc57a0ba218c6643fa81d0eb95360436
Pradip <dellpidarp57@gmail.com>
2024-04-21 18:48:55 +0545

    started homepage header just

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="p-5 bg-color3 flex justify-center">
+      <h1 className="text-color2 font-semibold text-4xl">🙏WELCOME🙏</h1>
+    </div>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color1": "#808080",
+        "color2": "#088178",
+        "color3": "#E3E6F3",
+        "color4":"black",
+        "color5":"white"
+        
+      },
+    },
   },
   plugins: [],
-}
+};
