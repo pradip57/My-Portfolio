8fb5feb40b13d28696996f4b5517ae29ab16865c
Pradip <dellpidarp57@gmail.com>
2024-05-01 19:14:55 +0545

    animation added from lottie animation

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,23 @@ import React from "react";
 import SectionTitle from "./SectionTitle";
 
 function About() {
-  return <div>
-    <SectionTitle title="About" />
-  </div>;
+  return (
+    <div>
+      <SectionTitle title="About" />
+
+      <div className="flex">
+        <div className="h-[50vh]">
+          <lottie-player
+            src="https://lottie.host/ad8489c6-54ce-44c9-a122-054401e2524d/4xAE3KxHck.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
