31fa99c2f7b614e0d18156029108b1c8b197e6f4
Pradip <dellpidarp57@gmail.com>
2024-05-01 18:55:37 +0545

    about section title designed

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function SectionTitle({ title }) {
   return (
-    <div className="flex">
-      <h1 className="text-2xl text-slate-600 font-semibold">{title}</h1>
+    <div className="flex gap-10 items-center py-10">
+      <h1 className="text-3xl text-slate-600 font-semibold">{title}</h1>
+      <div className="w-60 h-[2px] bg-color2"></div>
     </div>
   );
 }
